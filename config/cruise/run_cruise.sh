377ba6f40ac2dba0cf947f7c6b15f82f23127980
Scott Smith <scottnelsonsmith@gmail.com>
2012-09-07 16:14:23 -0700

    rvm wasn't configuring gemset before bundle install; corrected

--- config/cruise/run_cruise.sh
+++ config/cruise/run_cruise.sh
@@ -1,6 +1,6 @@
 /bin/bash -l -i -c "echo '*** starting Billing-Logic build ***' && \
-  bundle exec bundle install && \
-  billing && \
   source $HOME/.rvm/scripts/rvm && \
   source .rvmrc && \
+  bundle exec bundle install && \
+  billing && \
   bundle exec rake ci"
