dfd6141b6834610e1a13df52fce4335e0d645179
James Lavin <james@jameslavin.com>
2013-09-09 09:00:17 -0400

    rename tests to make clearer what they're doing

--- spec/billing_info/command_builders/command_builder_spec.rb
+++ spec/billing_info/command_builders/command_builder_spec.rb
@@ -6,35 +6,33 @@ describe BillingLogic::CommandBuilders::ActionObject do
     Time.zone = "Eastern Time (US & Canada)"
   end
 
-  it "recognizes a valid command to remove products from a bundle" do
-    # Remove two products from a bundle with one payment_profile
+  it "removes two products from a bundle with one payment profile" do
     command = "remove (B @ $20/mo & C @ $20/mo) from [(A @ $30/mo & B @ $20/mo & C @ $20/mo) @ $70/mo] now"
     BillingLogic::CommandBuilders::ActionObject.from_string(command).to_s.should == command
   end
 
-  it "recognizes a valid command to add two products" do
+  it "adds two new products" do
     command = "add (B @ $20/mo & C @ $20/mo) @ $40.00/mo now"
     translated_command = command.gsub("now", "on ") + Time.zone.now.strftime('%m/%d/%y')
     BillingLogic::CommandBuilders::ActionObject.from_string(command).to_s.should == translated_command
   end
 
-  it "recognizes a valid command to cancel a product" do
+  it "cancels a product" do
     command = "cancel [A @ $30/mo] now"
     BillingLogic::CommandBuilders::ActionObject.from_string(command).to_s.should == command
   end
 
-  it "recognizes a valid command to cancel a product (with parens)" do
-    # This is considered a bundle within Billing-Logic
+  it "cancels a product that is also a bundle (indicated by parentheses)" do
     command = "cancel [(A @ $30/mo) @ $30/mo] now"
     BillingLogic::CommandBuilders::ActionObject.from_string(command).to_s.should == command
   end
 
-  it "recognizes a valid command to add a product on a future date" do
+  it "adds a product on a future date" do
     command = "add (B @ $300/yr) @ $300.00/yr on 03/10/12"
     BillingLogic::CommandBuilders::ActionObject.from_string(command).to_s.should == command
   end
 
-  it "recognizes a valid command to cancel and disable a product" do
+  it "cancels and disables a product" do
     command = "cancel and disable [(A @ $30/mo & B @ $40/mo & C @ $25/mo) @ $95/mo] with refund $30.00 now"
     BillingLogic::CommandBuilders::ActionObject.from_string(command).to_s.should == command
   end
